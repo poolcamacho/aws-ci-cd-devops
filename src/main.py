67dd9e5b2b5f98abbba08e149f5a13fe70ea14e5
Pool Camacho <hello@poolcamacho.dev>
2025-02-03 19:10:52 -0700

    update test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 def add(a,b):
-    """Returns the sum of two numbers"""
+    """
+    Returns the sum of two numbers
+    """
     return a+b
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     print(f"2+3 = {add(2,3)}")
